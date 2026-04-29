da6e1ff846dad4abfdd7e8d30dedc911f2b8cc9a
frontalspy <frontalspy@gmail.com>
2026-04-29 12:59:44 +1000

    Initial commit

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,27 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+const extractMessage = (body) => {
+  const object = JSON.parse(body);
+  return object?.message || "";
 };
+
+const fetchQuotes = async (url) => {
+  try {
+    const response = await httpGet(url);
+    if (response.status === 200) {
+      return { "Arnie Quote": extractMessage(response.body) };
+    } else {
+      return { FAILURE: extractMessage(response.body) };
+    }
+  } catch (error) {
+    return error;
+  }
+};
+
+const getArnieQuotes = async (urls) =>
+  Promise.all(urls.map(fetchQuotes)).catch((error) => {
+    console.error("Error fetching Arnie quotes:", error);
+  });
 
 module.exports = {
   getArnieQuotes,
