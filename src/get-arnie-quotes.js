b4b51c73be1d863a0f353c1d5420480e4c6c2455
frontalspy <frontalspy@gmail.com>
2026-04-29 13:03:24 +1000

    makes promise return object more obvious

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -19,9 +19,11 @@ const fetchQuotes = async (url) => {
 };
 
 const getArnieQuotes = async (urls) =>
-  Promise.all(urls.map(fetchQuotes)).catch((error) => {
-    console.error("Error fetching Arnie quotes:", error);
-  });
+  Promise.all(urls.map(fetchQuotes))
+    .then((quotes) => quotes)
+    .catch((error) => {
+      console.error("Error fetching Arnie quotes:", error);
+    });
 
 module.exports = {
   getArnieQuotes,
